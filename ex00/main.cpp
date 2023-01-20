47950a328ab159e5dff6861cf65676d5aff34cc5
“brixxt27” <brixxt27@gmail.com>
2023-01-20 22:11:39 +0900

    feat: min 같을 때 두 번째 인자로 반환#2

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -18,5 +18,22 @@ int main( void ) {
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
+    // int n1 = 3;
+    // int n2 = 3; 
+    // int& ret1 = min(n1 , n2);
+    // int& ret2 = max(n1 , n2);
+
+    // ++ret1;
+    // std::cout << "n1 is " << n1 << "\nn2 is " << n2 << std::endl;
+    // std::cout << "---------------------------" << std::endl;
+    // ++ret2;
+    // std::cout << "n1 is " << n1 << "\nn2 is " << n2 << std::endl;
+
+    const int num1 = 3;
+    const int num2 = 3; 
+
+    std::cout <<  min(num1 , num2) << std::endl;
+    std::cout <<  max(num1 , num2) << std::endl;
+
     return 0;
 }
