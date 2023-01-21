0fff2e76c7b6f45367552a940b0411b1927fad2b
“brixxt27” <brixxt27@gmail.com>
2023-01-21 19:08:17 +0900

    feat: Array 템플릿 클래스 구현#7 #8
    - 템플릿 구현은 헤더에!
    - mArr = new T[n]으로 해야 할당이 받아지지, mArr = new T(n)으로 하면 T의
      생성자를 불러오려고 해서 SEGFAULT가 나오는 것 같다.

--- ex02/Array.h
+++ ex02/Array.h
@@ -21,4 +21,61 @@ private:
 	unsigned int	mSize;
 };
 
+template <typename T>
+Array<T>::Array()
+	: mSize(0) 
+{
+	mArr = new T[0];
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+	: mSize(n)
+{
+	mArr = new T[n];
+}
+
+template <typename T>
+Array<T>::Array(const Array& other)
+	: mSize(other.mSize)
+{
+	mArr = new T(mSize);
+
+	std::memcpy(mArr, other.mArr, mSize + 1);
+}
+
+template <typename T>
+Array<T>&	Array<T>::operator=(const Array& rhs)
+{
+	if (this == &rhs)
+		return *this;
+	
+	delete[] mArr;
+	mSize = rhs.mSize;
+	mArr = new T[mSize];
+
+	std::memcpy(mArr, rhs.mArr, mSize + 1);
+	return *this;
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] mArr;
+}
+
+template <typename T>
+T&	Array<T>::operator[](const unsigned int idx) const
+{
+	if (idx < 0 || idx > mSize - 1)
+		throw std::out_of_range("Out of range!");
+	return mArr[idx];
+}
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+	return mSize;
+}
+
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,12 +2,73 @@
 
 int	main()
 {
-	try {
-		Array<int>	arr;
+	//try {
+	//	Array<int>	arr;
 
-		std::cout << arr.size() << std::endl;
+	//	std::cout << "Size is " << arr.size() << std::endl;
+
+	//}
+	//catch (std::exception& e) {
+	//	std::cout << e.what() << std::endl;
+	//}
+
+	//std::cout << "\n-----------------------------------\n" << std::endl;
+
+	//try {
+	//	Array<int>	arr(5);
+
+	//	for ( unsigned int i = 0; i < arr.size(); i++) {
+	//		arr[i] = i;
+	//		std::cout << "idx " << i << ": " << arr[i] << std::endl;
+	//	}
+	//	std::cout << "Size is " << arr.size() << std::endl;
+
+	//	arr[-1] = 1;
+	//}
+	//catch (std::exception& e) {
+	//	std::cout << e.what() << std::endl;
+	//}
+
+	std::cout << "\n-----------------------------------\n" << std::endl;
+
+	try {
+		Array<int>	arr1(5);
+		Array<int>	arr2(6);
+
+		for ( unsigned int i = 0; i < arr1.size(); i++) {
+			arr1[i] = i + 1;
+			std::cout << "arr1's idx " << i << ": " << arr1[i] << std::endl;
+		}
+
+		arr2 = arr1;
+		for ( unsigned int i = 0; i < arr2.size(); i++) {
+			std::cout << "arr2's idx " << i << ": " << arr2[i] << std::endl;
+		}
+		std::cout << "Size is " << arr2.size() << std::endl;
 	}
 	catch (std::exception& e) {
 		std::cout << e.what() << std::endl;
 	}
+
+	std::cout << "\n-----------------------------------\n" << std::endl;
+
+	try {
+		Array<int>	arr1(5);
+		Array<int>	arr2(arr1);
+
+		for ( unsigned int i = 0; i < arr1.size(); i++) {
+			arr1[i] = i + 10;
+			std::cout << "arr1's idx " << i << ": " << arr1[i] << std::endl;
+		}
+
+		for ( unsigned int i = 0; i < arr2.size(); i++) {
+			arr2[i] = i;
+			std::cout << "arr2's idx " << i << ": " << arr2[i] << std::endl;
+		}
+		std::cout << "Size is " << arr2.size() << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+	//system("leaks array");
 }
