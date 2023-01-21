0d7a9ad2f9d9b3bd17fd520a5a19cfc4c460ab2b
“brixxt27” <brixxt27@gmail.com>
2023-01-21 14:44:22 +0900

    refactor: 들여쓰기를 모두 탭으로 변경, main 문 원래대로 수정

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,38 +2,38 @@
 #include "whatever.h"
 
 int main( void ) {
-    int a = 2;
-    int b = 3;
+	int a = 2;
+	int b = 3;
 
-    ::swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-    std::string c = "chaine1";
-    std::string d = "chaine2";
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
-    // int n1 = 3;
-    // int n2 = 3; 
-    // int& ret1 = min(n1 , n2);
-    // int& ret2 = max(n1 , n2);
+	// int n1 = 3;
+	// int n2 = 3; 
+	// int& ret1 = min(n1 , n2);
+	// int& ret2 = max(n1 , n2);
 
-    // ++ret1;
-    // std::cout << "n1 is " << n1 << "\nn2 is " << n2 << std::endl;
-    // std::cout << "---------------------------" << std::endl;
-    // ++ret2;
-    // std::cout << "n1 is " << n1 << "\nn2 is " << n2 << std::endl;
+	// ++ret1;
+	// std::cout << "n1 is " << n1 << "\nn2 is " << n2 << std::endl;
+	// std::cout << "---------------------------" << std::endl;
+	// ++ret2;
+	// std::cout << "n1 is " << n1 << "\nn2 is " << n2 << std::endl;
 
-    const int num1 = 3;
-    const int num2 = 3; 
+	// const int num1 = 3;
+	// const int num2 = 3; 
 
-    std::cout <<  min(num1 , num2) << std::endl;
-    std::cout <<  max(num1 , num2) << std::endl;
+	// std::cout <<  max(num1 , num2) << std::endl;
+	// std::cout <<  max(num1 , num2) << std::endl;
 
-    return 0;
+	return 0;
 }
--- ex00/whatever.h
+++ ex00/whatever.h
@@ -4,35 +4,35 @@
 template<typename T>
 void    swap(T& a, T& b)
 {
-    T   tmp;
+	T   tmp;
 
-    tmp = a;
-    a = b;
-    b = tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
 }
 
 template<typename T>
 T&  min(T& a, T& b)
 {
-    return (a < b ? a : b);
+	return (a < b ? a : b);
 }
 
 template<typename T>
 const T&  min(const T& a, const T& b)
 {
-    return (a < b ? a : b);
+	return (a < b ? a : b);
 }
 
 template<typename T>
 T&  max(T& a, T& b)
 {
-    return (a > b ? a : b);
+	return (a > b ? a : b);
 }
 
 template<typename T>
 const T&  max(const T& a, const T& b)
 {
-    return (a > b ? a : b);
+	return (a > b ? a : b);
 }
 
 #endif
